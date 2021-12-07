739e61ee505277f7134bdda7bf06849d54bb3ece
aayushsrivastava9760 <81965095+aayushsrivastava9760@users.noreply.github.com>
2021-12-07 14:56:45 +0530

    fixed the minor bug with name entry

--- scripts/main.js
+++ scripts/main.js
@@ -3,18 +3,28 @@ const name1 = document.getElementById("name1");
 const name2 = document.getElementById("name2");
 const cont = document.getElementById("cont");
 
-let check = 0;
+let player1_name = ''
+let player2_name = ''
+
+let player1_sym = ''
+let player2_sym = ''
+
+window.onload = function(){
+    localStorage.clear()
+}
 
 name1.onchange = function(){
-    localStorage.setItem('player1',name1.value);
-    check++;
-    if(check>=3) cont.setAttribute('class','');
+    if(name1.value !== ''){
+        player1_name = name1.value
+        check()
+    }
 };
 
 name2.onchange = function(){
-    localStorage.setItem('player2',name2.value);
-    check++;
-    if(check>=3) cont.setAttribute('class','');
+    if(name2.value !== ''){
+        player2_name = name2.value
+        check()
+    }
 };
 
 btn[0].onclick=function(){
@@ -22,39 +32,45 @@ btn[0].onclick=function(){
     btn[3].setAttribute('class','text-white text-4xl p-2 font-semibold bg-green-500 w-16 border-4 border-blue-700');
     btn[1].setAttribute('class','text-white text-4xl p-2 font-semibold bg-green-500 w-16 border-4 border-black');
     btn[2].setAttribute('class','text-white text-4xl p-2 font-semibold bg-green-500 w-16 border-4 border-black');
-    localStorage.setItem('sym1','X');
-    localStorage.setItem('sym2','O');
-    check++;
-    if(check>=3) cont.setAttribute('class','');
+    player1_sym = 'X'
+    player2_sym = 'O'
 };
+
+
 btn[1].onclick=function(){
     btn[1].setAttribute('class','text-white text-4xl p-2 font-semibold bg-green-500 w-16 border-4 border-blue-700');
     btn[2].setAttribute('class','text-white text-4xl p-2 font-semibold bg-green-500 w-16 border-4 border-blue-700');
     btn[0].setAttribute('class','text-white text-4xl p-2 font-semibold bg-green-500 w-16 border-4 border-black');
     btn[3].setAttribute('class','text-white text-4xl p-2 font-semibold bg-green-500 w-16 border-4 border-black');
-    localStorage.setItem('sym1','O');
-    localStorage.setItem('sym2','X');
-    check++;
-    if(check>=3) cont.setAttribute('class','');
+    player1_sym = 'O'
+    player2_sym = 'X'
 };
+
+
 btn[2].onclick=function(){
     btn[2].setAttribute('class','text-white text-4xl p-2 font-semibold bg-green-500 w-16 border-4 border-blue-700');
     btn[1].setAttribute('class','text-white text-4xl p-2 font-semibold bg-green-500 w-16 border-4 border-blue-700');
     btn[0].setAttribute('class','text-white text-4xl p-2 font-semibold bg-green-500 w-16 border-4 border-black');
     btn[3].setAttribute('class','text-white text-4xl p-2 font-semibold bg-green-500 w-16 border-4 border-black');
-    localStorage.setItem('sym1','O');
-    localStorage.setItem('sym2','X');
-    check++;
-    if(check>=3) cont.setAttribute('class','');
+    player1_sym = 'O'
+    player2_sym = 'X'
 };
 btn[3].onclick=function(){
     btn[0].setAttribute('class','text-white text-4xl p-2 font-semibold bg-green-500 w-16 border-4 border-blue-700');
     btn[3].setAttribute('class','text-white text-4xl p-2 font-semibold bg-green-500 w-16 border-4 border-blue-700');
     btn[1].setAttribute('class','text-white text-4xl p-2 font-semibold bg-green-500 w-16 border-4 border-black');
     btn[2].setAttribute('class','text-white text-4xl p-2 font-semibold bg-green-500 w-16 border-4 border-black');
-    localStorage.setItem('sym1','X');
-    localStorage.setItem('sym2','O');
-    check++;
-    if(check>=3) cont.setAttribute('class','');
+    player1_sym = 'X'
+    player2_sym = 'O'
 };
 
+
+const check = () =>{
+    if(player1_name !== '' && player2_name !== '' && player1_sym !=='' && player2_sym !==''){
+        cont.className = ''
+        localStorage.setItem('player1',player1_name)
+        localStorage.setItem('player2',player2_name)
+        localStorage.setItem('sym1',player1_sym)
+        localStorage.setItem('sym2',player2_sym)
+    }
+}
